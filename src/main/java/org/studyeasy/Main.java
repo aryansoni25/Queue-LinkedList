530cb7affa43b13bf891533108ff09ae7dbe64e4
aryansoni25 <ashoksoni9425@gmail.com>
2025-10-16 23:07:13 +0530

    Initial Commit!!

--- src/main/java/org/studyeasy/Main.java
+++ src/main/java/org/studyeasy/Main.java
@@ -3,6 +3,8 @@ package org.studyeasy;
 public class Main {
     public static void main(String[] args) {
         QueueLinkedList ql=new QueueLinkedList();
+        ql.enQueue(3);
+        ql.enQueue(4);
         ql.enQueue(5);
         ql.enQueue(6);
         ql.enQueue(7);
