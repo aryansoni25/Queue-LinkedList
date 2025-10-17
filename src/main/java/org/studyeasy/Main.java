893d6f4cfac84df082341ac5c41584d0085c0cf8
aryansoni25 <ashoksoni9425@gmail.com>
2025-10-17 18:19:44 +0530

    3rd Commit!!

--- src/main/java/org/studyeasy/Main.java
+++ src/main/java/org/studyeasy/Main.java
@@ -7,8 +7,9 @@ public class Main {
         ql.enQueue(4);
         ql.enQueue(5);
         ql.enQueue(6);
-        ql.enQueue(7);
         System.out.println(ql.peek());
+        ql.enQueue(7);
+        ql.enQueue(8);
         System.out.println(ql.deQueue());
     }
 }
